c7a091547a133c45caeeb530d76de97db99b5e63
Layeeq <layqahmed@gmail.com>
2026-02-06 16:47:51 -0600

    Init Spring Boot and maven config

---
+++ src/test/java/com/slippedpack/demo/taskmgmt/TaskmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slippedpack.demo.taskmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
